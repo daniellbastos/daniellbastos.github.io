f955f72b7594f9eb2557c1e2f1674d27ba4688b9
Daniel Bastos <danielfloresbastos@gmail.com>
2019-01-14 16:34:08 -0200

    reset blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,7 @@ PATH = 'content'
 
 TIMEZONE = 'America/Sao_Paulo'
 
-DEFAULT_LANG = u'pt'
+DEFAULT_LANG = 'pt'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,36 +20,13 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-DEFAULT_PAGINATION = 5
+DEFAULT_PAGINATION = 20
 
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+SUMMARY_MAX_LENGTH = 250
 
-THEME = 'themes'
+THEME = 'attila'
+RELATIVE_URLS = True
 
-MENUITEMS = [('Home', '/')]
-
-PLUGIN_PATHS = ['./plugins/pelican-plugins']
-PLUGINS = ['pelican_youtube']
-
+# plugins
 DISQUS_SITENAME = 'daniellbastos'
 GOOGLE_ANALYTICS = 'UA-39829332-1'
-
-# NEST Template Config
-NEST_CSS_MINIFY = True
-NEST_HEADER_IMAGES = ''
-NEST_HEADER_LOGO = '/theme/images/me.png'
-
-NEST_INDEX_HEAD_TITLE = 'Página inicial'
-NEST_INDEX_HEADER_TITLE = 'Meu Blog'
-NEST_INDEX_HEADER_SUBTITLE = SITESUBTITLE
-NEST_INDEX_CONTENT_TITLE = 'Últimos posts'
-
-NEST_AUTHOR_CONTENT_TITLE = 'Meus posts'
-
-NEST_PAGINATION_PREVIOUS = '<<'
-NEST_PAGINATION_NEXT = '>>'
-
-STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico']
-
-SHOW_FOOTER = False
